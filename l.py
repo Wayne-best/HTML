50a03a464f172715ca66c7995d631a2a2fd8dfb9
Wayne-best <waynem.m345@gmail.com>
2026-01-31 21:01:41 +0300

    Add files via upload

---
+++ l.py
@@ -0,0 +1,11 @@
+def no_notes(a):
+    Q = [2000,500,200,100,50,20,10]
+    x = 0
+    for i in range(7):
+        q = Q[i]
+        x = a//Q
+        print( "Notes of {} = {}. format"(q,x))
+        a= a%q
+        amount = int(input("Enter Total amount"))
+        no_notes(amount)
+        
